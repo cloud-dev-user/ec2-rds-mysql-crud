ef9bf02829b76ed6b6c11ef14d35aab381de8a4a
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2024-03-07 13:37:04 +0530

    Create ServletInitializer.java

---
+++ ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rudraksh.EC2RDScruddemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ec2RdsCrudDemoApplication.class);
+	}
+
+}
