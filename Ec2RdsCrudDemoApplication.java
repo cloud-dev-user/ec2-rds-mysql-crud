1be1ad964eeec2c9176a20be95143911961f15c5
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2024-03-07 13:36:27 +0530

    Create Ec2RdsCrudDemoApplication.java

---
+++ Ec2RdsCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rudraksh.EC2RDScruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2RdsCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2RdsCrudDemoApplication.class, args);
+	}
+
+}
